91bfe95c66e2811d6058e053659a3df2d0d751bd
memo <alexis45645@gmail.com>
2022-11-05 00:39:13 -0400

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,11 @@
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(hammingWeight(00000000000000000000000000001011));
+	}
+    public static int hammingWeight(int n) {
+        	return Integer.bitCount(n);
+    }
+}
